bba73c63355b01a347a775684e950195b3a5d134
Padraic Calpin <padraic.calpin93@gmail.com>
2016-02-23 16:12:00 +0000

    Correction for collision detection Added yaml requirement in travis.yml

--- boids.py
+++ boids.py
@@ -12,28 +12,36 @@ FIG_LIMITS = (-500., 1500.)
 class Flock(object):
 
     def __init__(self, number=50):
-        self.xs = [random.uniform(-450., 50.) for x in range(number)]
-        self.ys = [random.uniform(300., 600.) for x in range(number)]
-        self.xvs = [random.uniform(0., 10.) for x in range(number)]
-        self.yvs = [random.uniform(-20., 20.) for x in range(number)]
+        self.xs = np.array([random.uniform(-450., 50.) for x in range(number)])
+        self.ys = np.array([random.uniform(300., 600.) for x in range(number)])
+        self.xvs = np.array([random.uniform(0., 10.) for x in range(number)])
+        self.yvs = np.array([random.uniform(-20., 20.) for x in range(number)])
 
     @classmethod
     def from_data(cls, data):
         flock = cls()
+        data = [np.array(el) for el in data]
         flock.xs, flock.ys, flock.xvs, flock.yvs = data
         return flock
 
+    def get_tuple(self):
+        return (self.xs,self.ys,self.xvs,self.yvs)
+
     def update_boids(self):
+        # All of this smells of repetition even with numpy's help
         # Fly towards the middle
         self.xvs -= (self.xs - np.mean(self.xs))*0.01
         self.yvs -= (self.ys - np.mean(self.ys))*0.01
-
         # Fly away from nearby boids
-        for i in range(len(self.xs)):
-            for j in range(len(self.xs)):
-                if (self.xs[j]-self.xs[i])**2 + (self.ys[j]-self.ys[i])**2 < 100:
-                    self.xvs[i]=self.xvs[i]+(self.xs[i]-self.xs[j])
-                    self.yvs[i]=self.yvs[i]+(self.ys[i]-self.ys[j])
+        sep_x = self.xs[np.newaxis,:] - self.xs[:,np.newaxis]
+        sep_y = self.ys[np.newaxis,:] - self.ys[:,np.newaxis]
+        distant = (sep_x*sep_x + sep_y*sep_y) > 100.
+        x_correct = np.copy(sep_x)
+        x_correct[distant] = 0.
+        y_correct = np.copy(sep_y)
+        y_correct[distant] = 0.
+        self.xvs += np.sum(x_correct,0)
+        self.yvs += np.sum(y_correct,0)
         # Try to match speed with nearby boids
         for i in range(len(self.xs)):
             for j in range(len(self.xs)):
@@ -41,8 +49,8 @@ class Flock(object):
                     self.xvs[i]=self.xvs[i]+(self.xvs[j]-self.xvs[i])*0.125/len(self.xs)
                     self.yvs[i]=self.yvs[i]+(self.yvs[j]-self.yvs[i])*0.125/len(self.xs)
         # Move according to velocities
-        self.xs+= self.xvs
-        self.ys+= self.yvs
+        self.xs += self.xvs
+        self.ys += self.yvs
 
 flock = Flock()
 figure=plt.figure()
--- test_boids.py
+++ test_boids.py
@@ -10,15 +10,15 @@ def test_bad_boids_regression():
     boid_data=regression_data["before"]
     flock = Flock.from_data(boid_data)
     flock.update_boids()
-    res = (flock.xs,flock.ys,flock.xvs,flock.yvs)
-    for after,before in zip(regression_data["after"],res):
-        for after_value,before_value in zip(after,before):
-            assert_almost_equal(after_value,before_value,delta=0.01)
+    res = flock.get_tuple()
+    for after,calculated in zip(regression_data["after"],res):
+        for after_value,calculated_value in zip(after,calculated):
+            assert_almost_equal(after_value,calculated_value,delta=0.01)
 
 def test_from_data():
     data=yaml.load(open(os.path.join(os.path.dirname(__file__),'fixture.yml')))
     flock = Flock.from_data(data["before"])
-    imported = (flock.xs,flock.ys,flock.xvs,flock.yvs)
+    imported = flock.get_tuple()
     for real,got in zip(data["before"],imported):
         for real_value, got_value in zip(real,got):
             assert_equal(real_value,got_value)
