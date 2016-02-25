15dc7ae43b8428ce7197e6ccbde28344f478fc05
Padraic Calpin <padraic.calpin93@gmail.com>
2016-02-25 16:35:47 +0000

    Flock Config file To do: add testing for the config loader CLI

--- boids/boids.py
+++ boids/boids.py
@@ -5,27 +5,45 @@ for use as an exercise on refactoring.
 from matplotlib import pyplot as plt
 from matplotlib import animation
 import numpy as np
+import os
 import random
+import yaml
 
 FIG_LIMITS = (-500., 1500.)
 
 class Flock(object):
 
-    def __init__(self, number=50,**kwargs):
+    def __init__(self, **kwargs):
         data = kwargs.get('data', None)
+        conf = kwargs.get('conf',None)
+        self.load_conf(conf)
         if data is None:
-            xs = np.array([random.uniform(-450., 50.) for x in range(number)])
-            ys = np.array([random.uniform(300., 600.) for x in range(number)])
-            xvs = np.array([random.uniform(0., 10.) for x in range(number)])
-            yvs = np.array([random.uniform(-20., 20.) for x in range(number)])
+            xs = np.array([random.uniform(self.x_min, self.x_max) for x in range(number)])
+            ys = np.array([random.uniform(self.y_min, self.y_max) for x in range(number)])
+            xvs = np.array([random.uniform(self.xvs_min, self.xvs_max) for x in range(number)])
+            yvs = np.array([random.uniform(self.yvs_min, self.yvs_max) for x in range(number)])
         else:
             xs,ys,xvs,yvs = data
         self.positions = np.array([xs,ys])
         self.velocities = np.array([xvs,yvs])
         self.figure = plt.figure()
-        axes=plt.axes(xlim=FIG_LIMITS, ylim=FIG_LIMITS)
-        self.scatter=axes.scatter(xs, ys)
+        axes=plt.axes(xlim=self.FIG_LIMITS, ylim=self.FIG_LIMITS)
+        self.scatter = axes.scatter(xs, ys)
 
+    def load_conf(self, conf=None):
+        if conf == None:
+            with open(os.path.join(os.path.dirname(__file__),'config.yml'),'r') as f:
+                conf = yaml.load(f)
+        self.FIG_LIMITS = tuple(conf['animation_parameters'].get('fig_limits'))
+        self.frames = conf['animation_parameters']['frames']
+        self.interval = conf['animation_parameters']['interval']
+        self.flock_weight = conf['flock_parameters'].get('flocking_factor')
+        self.alert_distance = conf['flock_parameters'].get('alert_distance')
+        self.aware_distance = conf['flock_parameters'].get('aware_distance')
+        self.x_min, self.x_max = conf['boid_parameters'].get('x_window')
+        self.y_min, self.y_max = conf['boid_parameters'].get('y_window')
+        self.xvs_min, self.xvs_max = conf['boid_parameters'].get('xvs_window')
+        self.yvs_min, self.yvs_max = conf['boid_parameters'].get('yvs_window')
     @classmethod
     def from_data(cls, _data):
         flock = cls(data=_data)
@@ -44,17 +62,17 @@ class Flock(object):
     def update_boids(self):
         # Fly towards the middle
         flock_com = np.mean(self.positions,1)
-        self.velocities -= 0.01*(self.positions - flock_com[:,np.newaxis])
+        self.velocities -= self.flock_weight*(self.positions - flock_com[:,np.newaxis])
         # Fly away from nearby boids
         separations = self.positions[:,np.newaxis,:] - self.positions[:,:,np.newaxis]
-        distant = np.sum(separations*separations,0) > 100.
+        distant = np.sum(separations*separations,0) > self.alert_distance
         correction = np.copy(separations)
         correction[0,:,:][distant] = 0.
         correction[1,:,:][distant] = 0.
         self.velocities += np.sum(correction,1)
         # Try to match speed with nearby boids
         delta_vs = self.velocities[:,np.newaxis,:] - self.velocities[:,:,np.newaxis]
-        distant = np.sum(separations*separations,0) > 10000.
+        distant = np.sum(separations*separations,0) > self.aware_distance
         delta_vs[0,:,:][distant] = 0.
         delta_vs[1,:,:][distant] = 0.
         self.velocities += 0.125 * np.mean(delta_vs,1)
--- boids/tests/test_boids.py
+++ boids/tests/test_boids.py
@@ -2,6 +2,7 @@ import matplotlib
 matplotlib.use('Agg')
 from boids import Flock
 from nose.tools import assert_equal, assert_almost_equal
+import numpy as np
 import os
 import yaml
 
@@ -22,3 +23,14 @@ def test_from_data():
     for real,got in zip(data["before"],imported):
         for real_value, got_value in zip(real,got):
             assert_equal(real_value,got_value)
+
+def test_properties():
+    data=yaml.load(open(os.path.join(os.path.dirname(__file__),'fixture.yml')))
+    flock = Flock.from_data(data["after"])
+    xs,ys = flock.coord_tuple
+    got_data = flock.data
+    for after,got in zip(data["after"],got_data):
+        for after_value,got_value in zip(after,got):
+            assert_almost_equal(after_value,got_value,delta=0.01)
+    assert np.allclose(data["after"][0], xs)
+    assert np.allclose(data["after"][1], ys)
