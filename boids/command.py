086ef4a2e266d12db83c9bc33852717c4a32e31f
Padraic Calpin <padraic.calpin93@gmail.com>
2016-02-26 17:55:41 +0000

    Docstrings for boids.command

--- boids/command.py
+++ boids/command.py
@@ -1,4 +1,4 @@
-"""Defines argument parsing for use in """
+"""Defines argument parsing for use in the boids command line interface"""
 from boids import Flock
 import argparse
 import os
@@ -10,6 +10,7 @@ parse.add_argument('--example_config',action='store_true',help='Write out a defa
 parse.add_argument('-f','--from_file',
                    help="Load the flock parameters from a .yml file", metavar='config.yml')
 def process():
+    """Script called by the command line interface: Parses args and then runs a simulation"""
     args = parse.parse_args()
     if args.example_config:
         shutil.copyfile(os.path.join(os.path.dirname(__file__),'config.yml'),
